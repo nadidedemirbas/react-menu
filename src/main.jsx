e8d8b62c3b33d52b50731a3350cf65794a32981e
Nadide Demirbaş <nadidedemirbas@gmail.com>
2023-07-11 21:55:59 +0300

    Attach routes

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,78 @@ import {
 import './index.css'
 import ErrorPage from "./error-page.jsx";
 import Menu from "./routes/menu.jsx"
+import Android from "./routes/elektronik/android.jsx";
+import Apple from "./routes/elektronik/apple.jsx";
+import Dizustu from "./routes/elektronik/dizustu.jsx";
+import Masaustu from "./routes/elektronik/masaustu.jsx";
+import Elbise from "./routes/tekstil/elbise.jsx";
+import Tisort from "./routes/tekstil/tisort.jsx";
+import TopukluAyakkabi from "./routes/tekstil/topuklu-ayakkabi.jsx";
+import Sneaker from "./routes/tekstil/sneaker.jsx";
+import GozMakyaji from "./routes/kozmetik/goz-makyaji.jsx";
+import TenMakyaji from "./routes/kozmetik/ten-makyaji.jsx";
+import Sampuan from "./routes/kozmetik/sampuan.jsx";
+import SacKremi from "./routes/kozmetik/sac-kremi.jsx";
+
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Menu />,
         errorElement: <ErrorPage />,
-        // children: [
-        //     {
-        //         path: "contacts/:contactId",
-        //         element: <Contact />,
-        //     },
-        // ]
+        children: [
+            //Elektronik
+            {
+                path: "elektronik/android",
+                element: <Android />,
+            },
+            {
+                path: "elektronik/apple",
+                element: <Apple />,
+            },
+            {
+                path: "elektronik/dizustu",
+                element: <Dizustu />,
+            },
+            {
+                path: "elektronik/masaustu",
+                element: <Masaustu />,
+            },
+            //Tekstil
+            {
+                path: "tekstil/elbise",
+                element: <Elbise />,
+            },
+            {
+                path: "tekstil/tisort",
+                element: <Tisort />,
+            },
+            {
+                path: "tekstil/topuklu-ayakkabi",
+                element: <TopukluAyakkabi />,
+            },
+            {
+                path: "tekstil/sneaker",
+                element: <Sneaker />,
+            },
+            //Kozmetik
+            {
+                path: "kozmetik/goz-makyaji",
+                element: <GozMakyaji />,
+            },
+            {
+                path: "kozmetik/ten-makyaji",
+                element: <TenMakyaji />,
+            },
+            {
+                path: "kozmetik/sampuan",
+                element: <Sampuan />,
+            },
+            {
+                path: "kozmetik/sac-kremi",
+                element: <SacKremi />,
+            }
+        ]
     },
 
 ]);
